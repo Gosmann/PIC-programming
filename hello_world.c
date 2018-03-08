3e826b1c1d8e7f45a2b8b49565d9376b951a25da
Gabriel Gosmann <gabriel.gosmann@gmail.com>
2018-03-08 14:57:51 -0300

    Create hello_world.c

---
+++ hello_world.c
@@ -0,0 +1,18 @@
+// description:
+//
+// source:
+
+void main(){
+     // config PORTB and TRISB - PORTB AND TRISB REGISTERS (pg. 47 PIC16F887 datasheet)
+     // PORTB
+     TRISB = 0x00;         // configure all PORTB as digital output
+     PORTB = 0x00;         // configure all PORTB as LOW
+     
+     while(1){             // infinite loop
+          PORTB = 0x00;    // sets all PORTB as LOW
+          delay_ms(200);   // waits 200ms
+          PORTB = 0xFF;    // sets all PORTB as HIGH
+          delay_ms(200);   // waits 200ms
+
+     } // end while
+} // end main
